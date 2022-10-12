44de2c9b67779aec0988e9ec201f986062821e55
eleonora <nebrudko18@gmail.com>
2022-10-12 14:21:51 +0300

    added alert

---
+++ src/components/AppAlert.vue
@@ -0,0 +1,74 @@
+<template>
+	<div
+		class="alert d-flex justify-content-center align-items-start"
+		role="alert"
+		:class="[`alert-${color}`]"
+	>
+		<div class="mx-3">
+			<h6 class="alert-title">{{ title }}</h6>
+			<p class="m-0">{{ text }}</p>
+		</div>
+		<CloseButton v-if="closable" @click="$emit('close')" size="xs" />
+	</div>
+</template>
+
+<script>
+import CloseButton from "../components/CloseButton.vue";
+
+export default {
+	name: "AppAlert",
+	components: {
+		CloseButton,
+	},
+	emits: ["close"],
+	props: {
+		title: {
+			type: String,
+			default: "I am a title component",
+		},
+		text: {
+			type: String,
+			default: "I am a text component",
+		},
+		color: {
+			type: String,
+			requeired: false,
+			default: "primary",
+			validator(val) {
+				return [
+					"secondary",
+					"success",
+					"danger",
+					"warning",
+					"info",
+					"light",
+					"dark",
+				].includes(val);
+			},
+		},
+		closable: {
+			type: Boolean,
+			requeired: false,
+			default: false,
+		},
+	},
+};
+</script>
+
+
+<style scoped lang="scss">
+h3 {
+	margin: 40px 0 0;
+}
+ul {
+	list-style-type: none;
+	padding: 0;
+}
+li {
+	display: inline-block;
+	margin: 0 10px;
+}
+a {
+	color: #42b983;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,44 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="container">
+		<div class="">
+			<div class="d-flex justify-content-center align-items-center">
+				<AppAlert
+					v-if="alert"
+					title="Important information!"
+					text="Be carefull"
+					color="success"
+					class="border border-light"
+					:closable="true"
+					@close="alert = false"
+				/>
+			</div>
+			<MyButtons @click="alert = true" color="success" size="sm"
+				>Show message</MyButtons
+			>
+		</div>
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import AppAlert from "../components/AppAlert.vue";
+import MyButtons from "../components/MyButtons.vue";
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+	name: "HomeView",
+	components: {
+		AppAlert,
+		MyButtons,
+	},
+	data() {
+		return {
+			alert: false,
+		};
+	},
+};
 </script>
+
+<style lang="scss" scoped>
+.width-alert {
+	width: 40%;
+}
+</style>
