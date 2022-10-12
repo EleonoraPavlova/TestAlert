554858eef68e8e684e5541a2854c6a34f629886e
eleonora <nebrudko18@gmail.com>
2022-10-12 14:22:10 +0300

    added alert

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import "./assets/normalize.css";
+import "./assets/base.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+//подключено через терминал
 
 createApp(App).use(store).use(router).mount('#app')
